a5725ba5959fff24fbc1aad0e255dafea65e09d1
Danhlocnd <danhloc.nd@gmail.com>
2022-09-09 11:26:46 +0700

    sdasdsa

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,11 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 import 'dart:async';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:flutter_background_service_android/flutter_background_service_android.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:path_provider/path_provider.dart';
@@ -30,58 +34,26 @@ final BehaviorSubject<String?> selectNotificationSubject =
 void callbackDispatcher() {
   Workmanager().executeTask((task, inputData) async {
     if (task == 'uniquekey') {
-      // connectAndListen();
-      // final httpsUri = Uri(
-      //     scheme: 'https',
-      //     host: 'reqres.in',
-      //     path: 'api/users/2'
-      // );
-      // var response = await http.get(httpsUri);
-      // Map data = jsonDecode(response.body);
-      // final FlutterLocalNotificationsPlugin flutterlocal =
-      //     FlutterLocalNotificationsPlugin();
-      // const AndroidNotificationDetails androidNotificationDetails =
-      //     AndroidNotificationDetails(
-      //         "channelId danhloc", "channelName android Noti",
-      //         importance: Importance.max,
-      //         priority: Priority.high,
-      //         showWhen: false);
-      // const NotificationDetails platfromChanel =
-      //     NotificationDetails(android: androidNotificationDetails);
-      //
-      // await flutterlocal.show(0123, "data['data']['first_name']",
-      //     "data['data']['email']", platfromChanel,
-      //     payload: 'item x ');
+      connectAndListen();
 
-      // final _channel = IOWebSocketChannel.connect(
-      //   Uri.parse('wss://demo.piesocket.com/v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self'),
-      // );
-      // _channel.stream.listen((message) async {
-      //
-      //   print(message);
-      //   // if(message == "Hello world!"){
-      //     final FlutterLocalNotificationsPlugin flutterlocal =
-      //     FlutterLocalNotificationsPlugin();
-      //     const AndroidNotificationDetails androidNotificationDetails =
-      //     AndroidNotificationDetails(
-      //         "channelId danhloc", "channelName android Noti",
-      //         importance: Importance.max,
-      //         priority: Priority.high,
-      //         showWhen: false);
-      //     const NotificationDetails platfromChanel =
-      //     NotificationDetails(android: androidNotificationDetails);
-      //
-      //     await flutterlocal.show(0123, "data['data']['first_name']",
-      //         "data['data']['email']", platfromChanel,
-      //         payload: 'item x ');
-      //   // }
-      //   // Fluttertoast.showToast(msg: "$message", toastLength: Toast.LENGTH_SHORT);
-      //   // _channel.sink.add('received!');
-      //   // _channel.sink.close(status.goingAway);
-      // });
     }
     return Future.value(true);
   });
+}
+
+show(String msg) async {
+  final FlutterLocalNotificationsPlugin flutterlocal =
+      FlutterLocalNotificationsPlugin();
+  const AndroidNotificationDetails androidNotificationDetails =
+      AndroidNotificationDetails(
+          "channelId danhloc", "channelName android Noti",
+          importance: Importance.max, priority: Priority.high, showWhen: false);
+  const NotificationDetails platfromChanel =
+      NotificationDetails(android: androidNotificationDetails);
+  var ramdom = Random(10);
+  await flutterlocal.show(
+      ramdom.nextInt(10000), msg, "data['data']['email']", platfromChanel,
+      payload: 'item x ');
 }
 
 void main() async {
@@ -107,40 +79,163 @@ void main() async {
           true // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
       );
 
-  // Workmanager().registerPeriodicTask(
-  //   "uniquekey",
-  //   "uniquekey",
-  //   // initialDelay: const Duration(seconds: 10),
-  // );
 
+  await initializeService();
   runApp(const MyApp());
 }
 
+Future<void> initializeService() async {
+  final service = FlutterBackgroundService();
+  await service.configure(
 
-IO.Socket socket = IO.io('http://192.168.101.20:5000',OptionBuilder()
-    .setTransports(['websocket']).disableAutoConnect() // disable auto-connection// for Flutter or Dart VM
-    .build());
-void connectAndListen() {
+    androidConfiguration: AndroidConfiguration(
+      // this will be executed when app is in foreground or background in separated isolate
+      onStart: onStart,
 
-  socket.connect();
+      // auto start service
+      autoStart: true,
+      isForegroundMode: true,
+    ),
+    iosConfiguration: IosConfiguration(
+      // auto start service
+      autoStart: true,
 
-  // socket.emit("signin", widget.sourchat.id);
-  socket.on('hello', (arg) => {print(arg)});
+      // this will be executed when app is in foreground in separated isolate
+      onForeground: onStart,
 
-  socket.onConnect((data) {
-    print("connect");
-    socket.on("message", (msg) {
-      print(msg);
-      socket.emit("message", {
-        "message": "message",
-        "sourceId": "sourceId",
-        "targetId": "targetId"
-      });
+      // you have to enable background fetch capability on xcode project
+      onBackground: onIosBackground,
+
+    ),
+  );
+  service.startService();
+}
+
+// to ensure this is executed
+// run app from xcode, then from xcode menu, select Simulate Background Fetch
+bool onIosBackground(ServiceInstance service) {
+  WidgetsFlutterBinding.ensureInitialized();
+  print('FLUTTER BACKGROUND FETCH');
+
+  return true;
+}
+
+void onStart(ServiceInstance service) async {
+
+  // Only available for flutter 3.0.0 and later
+  DartPluginRegistrant.ensureInitialized();
+
+  // For flutter prior to version 3.0.0
+  // We have to register the plugin manually
+
+  connectAndListen();
+  // SharedPreferences preferences = await SharedPreferences.getInstance();
+  // await preferences.setString("hello", "world");
+
+  if (service is AndroidServiceInstance) {
+    service.on('setAsForeground').listen((event) {
+      service.setAsForegroundService();
+      // connectAndListen();
     });
+
+    service.on('setAsBackground').listen((event) {
+      service.setAsBackgroundService();
+      // connectAndListen();
+    });
+  }
+
+  service.on('stopService').listen((event) {
+    service.stopSelf();
   });
 
+  // // // bring to foreground
+  // Timer.periodic(const Duration(seconds: 1), (timer) async {
+  //   // final hello = preferences.getString("hello");
+  //   // print(hello);
+  //
+  //   if (service is AndroidServiceInstance) {
+  //     service.setForegroundNotificationInfo(
+  //       title: "My App Service",
+  //       content: "Updated at ${DateTime.now()}",
+  //     );
+  //   }
+  //
+  //   /// you can see this log in logcat
+  //   print('FLUTTER BACKGROUND SERVICE: ${DateTime.now()}');
+  //
+  //   // test using external plugin
+  //   final deviceInfo = DeviceInfoPlugin();
+  //   String? device;
+  //   if (Platform.isAndroid) {
+  //     final androidInfo = await deviceInfo.androidInfo;
+  //     device = androidInfo.model;
+  //   }
+  //
+  //   if (Platform.isIOS) {
+  //     final iosInfo = await deviceInfo.iosInfo;
+  //     device = iosInfo.model;
+  //   }
+  //
+  //   service.invoke(
+  //     'update',
+  //     {
+  //       "current_date": DateTime.now().toIso8601String(),
+  //       "device": device,
+  //     },
+  //   );
+  // });
+}
+ connectAndListen() async {
+
+  IO.Socket socket = IO.io(
+      'http://192.168.101.20:5000',
+      OptionBuilder()
+          .setTransports(['websocket'])
+          .disableAutoConnect() // disable auto-connection// for Flutter or Dart VM
+          .build());
+
+  if(socket.connected == false) {
+    socket.connect();
+
+    final FlutterLocalNotificationsPlugin flutterlocal =
+    FlutterLocalNotificationsPlugin();
+    const AndroidNotificationDetails androidNotificationDetails =
+    AndroidNotificationDetails(
+        "channelId danhloc", "channelName android Noti",
+        importance: Importance.max, priority: Priority.high, showWhen: false);
+    const NotificationDetails platfromChanel =
+    NotificationDetails(android: androidNotificationDetails);
+    var ramdom = Random(10).nextInt(10000);
+    socket.onConnect((data) async {
+      print("connect");
+      Fluttertoast.showToast(
+          msg: "connect socket", toastLength: Toast.LENGTH_LONG);
+      socket.on(
+          'message',
+              (msg) async =>
+          {
+            await flutterlocal.show(ramdom, msg,
+                "data['data']['email']", platfromChanel,
+                payload: 'item x ')
+          });
+        // test using external plugin
+        final deviceInfo = DeviceInfoPlugin();
+        String? device;
+        if (Platform.isAndroid) {
+          final androidInfo = await deviceInfo.androidInfo;
+          device = androidInfo.model;
+        }
+
+        if (Platform.isIOS) {
+          final iosInfo = await deviceInfo.iosInfo;
+          device = iosInfo.model;
+        }
+      socket.emit('info',device);
+    });
+  }
   socket.onConnectError((data) {
     log(data);
+    connectAndListen();
   });
 
   socket.onError((data) {
@@ -207,11 +302,7 @@ class _MyAppState extends State<MyApp> {
             ),
             floatingActionButton: FloatingActionButton(
               onPressed: () {
-                socket.emit("message", {
-                  "message": "message",
-                  "sourceId": "sourceId",
-                  "targetId": "targetId"
-                });
+
               },
               tooltip: 'Send message',
               child: const Icon(Icons.send),
